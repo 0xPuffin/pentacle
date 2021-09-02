0449883e6b668313745f2108875acf108b79a755
pentacle <pentacle@pentacle.xyz>
2021-09-02 02:11:02 +0100

    clean up bad things screen (#36)

--- src/components/tx-cards/tx-card-med.js
+++ src/components/tx-cards/tx-card-med.js
@@ -9,20 +9,20 @@ const TxCardMed = () => {
     useEffect(() => {
         const getData = async () => {
 
-          if (!chat.length) {
+            if (!chat.length) {
 
-            const json = await fetch('https://raw.githubusercontent.com/penta-fun/polynetwork-hacker-chat/main/data.json')
-                .then(res => res.json())
-                .then(res => res.reverse())
-                .then(res => res.map(item => {
-                    return {
-                        ...item,
-                        timestamp: new Date((item.timestamp || 1628695418) * 1000)
-                    }
-                }))
-            setChat(json);
-          }
-          setFilteredChat(chat);
+                const json = await fetch('https://raw.githubusercontent.com/penta-fun/polynetwork-hacker-chat/main/data.json')
+                    .then(res => res.json())
+                    .then(res => res.reverse())
+                    .then(res => res.map(item => {
+                        return {
+                            ...item,
+                            timestamp: new Date((item.timestamp || 1628695418) * 1000)
+                        }
+                    }))
+                setChat(json);
+            }
+            setFilteredChat(chat);
         }
         getData();
     }, [chat]);
@@ -32,33 +32,27 @@ const TxCardMed = () => {
     }, [chat, hackerOnly])
 
     return (
-        <section className={"margin-top-1"}>
-            <article className={"max-width-50 margin-auto"}>
-                <div className={"field"}>
-                    <label htmlFor="hacker">
+        <article classame={"box feature light box-padding margin-top-1"}>
+            <div className={"field"}>
+                <label htmlFor="hacker">
                     <input type="checkbox" id="hacker" onClick={() => {
                         setHackerOnly(!hackerOnly)
                     }}/> <span className={"haxorgreen"}>Hacker messages</span></label>
-                </div>
-                {filteredChat
-                    .map((item, i) => <a key={i}
-                                         href={`https://etherscan.io/tx/${item.hash}`} target={"_blank"}
-                                         rel="noopener noreferrer">
-                            <div className="box feature dark link box-padding padding-bottom-0-5">
-                                <div
-                                    className={item.address === '0xC8a65Fadf0e0dDAf421F28FEAb69Bf6E2E589963' ? 'haxorgreen' : 'info'}>
-                                    <p>{item.message === '[2m(unintelligible)[0m' ? '(unintelligible)' : item.message}</p>
-                                    <span className={"txt-opacity-50"}>{item.address}</span>
-
-
-                                    <br/>
-                                    <span>{item.timestamp.toLocaleString()}</span>
-                                </div>
+            </div>
+            {filteredChat
+                .map((item, i) => <a key={i}
+                                     href={`https://etherscan.io/tx/${item.hash}`} target={"_blank"}
+                                     rel="noopener noreferrer">
+                        <div className="box feature light link box-padding margin-bottom-1">
+                            <div className={item.address === '0xC8a65Fadf0e0dDAf421F28FEAb69Bf6E2E589963' ? 'haxorgreen' : 'info'}>
+                                <p className={"padding-y-0-5"}>{item.message === '[2m(unintelligible)[0m' ? '(unintelligible)' : item.message}</p>
+                                <div className={"txt-opacity-50"}>{item.address}</div>
+                                <div>{item.timestamp.toLocaleString()}</div>
                             </div>
-                        </a>
-                    )}
-            </article>
-        </section>
+                        </div>
+                    </a>
+                )}
+        </article>
     );
 };
 
--- src/pages/bad-things.js
+++ src/pages/bad-things.js
@@ -5,14 +5,27 @@ import TxCardMed from "../components/tx-cards/tx-card-med";
 
 export const BadThings = () => {
     return (
-        <React.Fragment>
+        <>
             <Header/>
             <Layout>
-                <h1>Bad things™ 1.0 - Poly Network hack</h1>
-                <p className={"align-center"}><span>10-Aug-2021</span> | <span>Lilac bunny ended up coding <a className={"text-link"} href={"https://gist.github.com/banteg/bc9a6d5594fac27fa1d9342421fa570d"} target={"_blank"} rel="noopener noreferrer">this</a> to read the hacker's on-chain blog</span> | <span><a className={"text-link"} href={"https://twitter.com/kelvinfichter/status/1425217046636371969"} target={"_blank"} rel="noopener noreferrer">Post-mortem by @kelvinfichter</a></span></p>
+                <section className={"editorial-content two-col"}>
+                    <article className={"box feature dark box-padding margin-top-1 hero"}>
+                        <h1>Bad things™ 1.0 - Poly Network hack</h1>
+                        <p className={"align-center"}><span>10-Aug-2021</span> | <span>Lilac bunny ended up <a
+                            className={"text-link"}
+                            href={"https://gist.github.com/banteg/bc9a6d5594fac27fa1d9342421fa570d"} target={"_blank"}
+                            rel="noopener noreferrer">coding this</a> to read the hacker's on-chain blog</span> | <span><a
+                            className={"text-link"}
+                            href={"https://twitter.com/kelvinfichter/status/1425217046636371969"} target={"_blank"}
+                            rel="noopener noreferrer">Post-mortem by @kelvinfichter</a></span>
+                        </p>
+                    </article>
                     <TxCardMed/>
-                <p className={"margin-top-1"}>Bad things™ naming credit <a className={"text-link"} href={"https://twitter.com/tayvano_"}>Tayvano</a></p>
+                    <p className={"margin-top-1"}>Bad things™ naming credit <a className={"text-link"}
+                                                                               href={"https://twitter.com/tayvano_"}>Tayvano</a>
+                    </p>
+                </section>
             </Layout>
-        </React.Fragment>
+        </>
     )
 }
