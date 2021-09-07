bfead13dd96e529a4d9ad0ea04b65655c70082fb
pentcle <pentacle@pentacle.xyz>
2021-09-07 02:58:41 +0100

    update logo
    Signed-off-by: pentcle <pentacle@pentacle.xyz>

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ class Header extends Component {
     render() {
         return <header className={"p-header"}>
             <div className="header-content flex justify-end align-items-center">
-                <div><Link to="/"><img alt="Pentacle logo" className={"logo"} src={"pentacle-logo-2.svg"}/></Link></div>
+                <div><Link to="/"><img alt="Pentacle logo" className={"logo"} src={"pentacle-logo-LH.svg"}/></Link></div>
                 <div>In your quest for knowledge, a talisman of protection</div>
             </div>
         </header>
