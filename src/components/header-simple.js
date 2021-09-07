aec50494df6066ba1757a213889f40efb852b49c
pentcle <pentacle@pentacle.xyz>
2021-09-07 02:44:20 +0100

    new json
    Signed-off-by: pentcle <pentacle@pentacle.xyz>

--- src/components/header-simple.js
+++ src/components/header-simple.js
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 class HeaderSimple extends Component {
     render() {
         return <>
-            <div className={"p-header flex-1"}>
+            <div className={"p-header-simple flex-1"}>
                 <Link to="/"><img alt="Pentacle Logo" className={"logo"} src={"pentacle-logo-2.svg"}/></Link>
             </div>
         </>
