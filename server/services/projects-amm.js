daaf656403e69319637815204b92a4bceb4f4f2d
pentcle <pentacle@pentacle.xyz>
2021-11-26 13:13:12 +0000

    add more things to db query for amm
    Signed-off-by: pentcle <pentacle@pentacle.xyz>

--- server/services/projects-amm.js
+++ server/services/projects-amm.js
@@ -2,10 +2,26 @@ const db = require('./db');
 const helper = require('../../utils/helper');
 const config = require('../../config');
 
+// async function getMultiple(page = 1) {
+//     const offset = helper.getOffset(page, config.listPerPage);
+//     const rows = await db.query(
+//         'SELECT * FROM project JOIN project_tag ON project_tag.tag_id =1 OFFSET $1 LIMIT $2',    // tag_id 1 is amm, hardcorded zz
+//         [offset, config.listPerPage]
+//     );
+//     const data = helper.emptyOrRows(rows);
+//     const meta = {page};
+//
+//     return {
+//         data,
+//         meta
+//     }
+// }
+
 async function getMultiple(page = 1) {
     const offset = helper.getOffset(page, config.listPerPage);
     const rows = await db.query(
-        'SELECT * FROM project INNER JOIN project_tag ON project_tag.tag_id = 1 OFFSET $1 LIMIT $2',    // tag_id 1 is amm, hardcorded zz
+        // 'SELECT * FROM project INNER JOIN project_tag ON project_tag.tag_id = 1 OFFSET $1 LIMIT $2',    // tag_id 1 is amm, hardcorded zz
+        'SELECT project.project_id, project.project_name, project.description, tag.tag_name FROM project JOIN project_tag ON project.project_id = project_tag.project_id JOIN tag ON tag.tag_id = project_tag.tag_id WHERE tag.tag_id = 1 OFFSET $1 LIMIT $2',    // tag_id 1 is amm, hardcorded zz
         [offset, config.listPerPage]
     );
     const data = helper.emptyOrRows(rows);
@@ -25,22 +41,6 @@ function validateCreate(project) {
     if (!project) {
         messages.push('No object is provided');
     }
-
-    // if (!project.quote) {
-    //     messages.push('Quote is empty');
-    // }
-    //
-    // if (!project.author) {
-    //     messages.push('Author is empty');
-    // }
-    //
-    // if (project.quote && quote.quote.length > 255) {
-    //     messages.push('Quote cannot be longer than 255 characters');
-    // }
-    //
-    // if (project.author && quote.author.length > 255) {
-    //     messages.push('Author name cannot be longer than 255 characters');
-    // }
 
     if (messages.length) {
         let error = new Error(messages.join());
