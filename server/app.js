38f1ac046405c66077a32c42b1ce66bd26ce7f32
pentcle <pentacle@pentacle.xyz>
2021-12-01 13:38:16 +0000

    let's try this now
    Signed-off-by: pentcle <pentacle@pentacle.xyz>

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
-const indexRouter = require("./routes/index");
+const indexRouter = require("./routes/projects");
 // const projectsRouter = require("./routes/projects");
 // const ethAmmRouter = require("./routes/projects-amm");
 // const ethDexRouter = require("./routes/eth-decentralised-exchange");
