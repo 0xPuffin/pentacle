86274383953fab4c3b03fbf1027384c6ee9e38a1
pentcle <pentacle@pentacle.xyz>
2021-12-01 14:30:18 +0000

    ssl config
    Signed-off-by: pentcle <pentacle@pentacle.xyz>

--- server/config.js
+++ server/config.js
@@ -17,6 +17,7 @@ const env = process.env;
 const config = {
     db: {
         connectionString: process.env.DATABASE_URL,
+        rejectUnauthorized: false
     },
     listPerPage: env.LIST_PER_PAGE || 200,
 };
