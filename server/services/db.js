9985c800baa634432c764e8994854fbe0bac9f27
pentcle <pentacle@pentacle.xyz>
2021-12-01 12:42:33 +0000

    move config to server dir
    Signed-off-by: pentcle <pentacle@pentacle.xyz>

--- server/services/db.js
+++ server/services/db.js
@@ -1,8 +1,5 @@
-/**
- * use this for local dev
- */
 const { Pool } = require("pg");
-const config = require("../../config");
+const config = require("../config");
 const pool = new Pool(config.db);
 
 /**
