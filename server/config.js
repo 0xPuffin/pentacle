4600327c917e18bb5aead71b4257e95165d2e42a
pentcle <pentacle@pentacle.xyz>
2021-12-15 13:20:20 +0000

    config

--- server/config.js
+++ server/config.js
@@ -1,27 +1,27 @@
 const env = process.env;
 
-// const config = {
-//     db: {
-//         host: env.DB_HOST || 'localhost',
-//         port: env.DB_PORT || '5432',
-//         user: env.DB_USER || 'postgres',
-//         password: env.DB_PASSWORD || 'postgres',
-//         database: env.DB_NAME || '5432',
-//     },
-//     listPerPage: env.LIST_PER_PAGE || 200,
-// };
+const config = {
+    db: {
+        host: env.DB_HOST || 'localhost',
+        port: env.DB_PORT || '5432',
+        user: env.DB_USER || 'postgres',
+        password: env.DB_PASSWORD || 'postgres',
+        database: env.DB_NAME || 'pentaclexyz',
+    },
+    listPerPage: env.LIST_PER_PAGE || 200,
+};
 
 /**
  * config for prod
  */
-const config = {
-    db: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false
-        }
-    },
-    listPerPage: env.LIST_PER_PAGE || 200,
-};
+// const config = {
+//     db: {
+//         connectionString: process.env.DATABASE_URL,
+//         ssl: {
+//             rejectUnauthorized: false
+//         }
+//     },
+//     listPerPage: env.LIST_PER_PAGE || 200,
+// };
 
 module.exports = config;
