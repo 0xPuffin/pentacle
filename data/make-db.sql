ceff0f47baf264f9ddbb40fe81224b83048220f1
pentcle <pentacle@pentacle.xyz>
2021-12-20 13:26:09 +0000

    add education to db

--- data/make-db.sql
+++ data/make-db.sql
@@ -3,57 +3,71 @@ DROP TABLE tag;
 DROP TABLE project_tag;
 DROP TABLE article;
 DROP TABLE article_tag;
-CREATE TABLE project (
-  project_id      serial primary key,
-  name    varchar(4000),
-  coingecko_id    varchar(400),
-  description    varchar(4000),
-  token_image_url    varchar(4000),
-  ticker    varchar(4000),
-  website_url    varchar(4000),
-  webapp_url    varchar(4000),
-  contract_url    varchar(4000),
-  coingecko_url    varchar(4000),
-  llama_url    varchar(4000),
-  dune_url    varchar(4000),
-  rekt_url    varchar(4000),
-  whitepaper_url    varchar(4000),
-  github_url    varchar(4000),
-  docs_url    varchar(4000),
-  twitter_url    varchar(4000),
-  discord_url    varchar(4000),
-  telegram_url    varchar(4000),
-  medium_url    varchar(4000)
+DROP TABLE education;
+DROP TABLE education_tag;
+CREATE TABLE project
+(
+    project_id      serial primary key,
+    name            varchar(4000),
+    coingecko_id    varchar(400),
+    description     varchar(4000),
+    token_image_url varchar(4000),
+    ticker          varchar(4000),
+    website_url     varchar(4000),
+    webapp_url      varchar(4000),
+    contract_url    varchar(4000),
+    coingecko_url   varchar(4000),
+    llama_url       varchar(4000),
+    dune_url        varchar(4000),
+    rekt_url        varchar(4000),
+    whitepaper_url  varchar(4000),
+    github_url      varchar(4000),
+    docs_url        varchar(4000),
+    twitter_url     varchar(4000),
+    discord_url     varchar(4000),
+    telegram_url    varchar(4000),
+    medium_url      varchar(4000)
 );
-CREATE TABLE tag (
-  tag_id      serial primary key,
-  name    varchar(400),
-  description   varchar(4000)
+CREATE TABLE tag
+(
+    tag_id      serial primary key,
+    name        varchar(400),
+    description varchar(4000)
 );
-CREATE TABLE project_tag (
-  project_id      int,
-  tag_id    	  int
+CREATE TABLE project_tag
+(
+    project_id int,
+    tag_id     int
 );
-CREATE TABLE article (
-  article_id      serial primary key,
-  title    varchar(4000),
-  article_url   varchar(4000)
+CREATE TABLE article
+(
+    article_id  serial primary key,
+    title       varchar(4000),
+    article_url varchar(4000)
 );
-CREATE TABLE article_tag (
-  article_id      int,
-  tag_id    	  int
+CREATE TABLE article_tag
+(
+    article_id int,
+    tag_id     int
 );
-
-
--- COPY project FROM '/Users/pentaclexyz/projects/pentacle/data/project-no-tags.csv' delimiter ',' CSV HEADER;
--- COPY tag FROM '/Users/pentaclexyz/projects/pentacle/data/tag.csv'delimiter ',' CSV HEADER;
--- COPY project_tag FROM '/Users/pentaclexyz/projects/pentacle/data/project_tags.csv'delimiter ',' CSV HEADER;
--- COPY article FROM '/Users/pentaclexyz/projects/pentacle/data/article-no-tags.csv'delimiter ',' CSV HEADER;
--- COPY article_tag FROM '/Users/pentaclexyz/projects/pentacle/data/article_tags.csv'delimiter ',' CSV HEADER;
-
-
--- COPY project FROM '/Users/serranged/Documents/GitHub/pentacle/data/project-no-tags.csv' delimiter ',' CSV HEADER;
--- COPY tag FROM '/Users/serranged/Documents/GitHub/pentacle/data/tag.csv'delimiter ',' CSV HEADER;
--- COPY project_tag FROM '/Users/serranged/Documents/GitHub/pentacle/data/project_tags.csv'delimiter ',' CSV HEADER;
--- COPY article FROM '/Users/serranged/Documents/GitHub/pentacle/data/article-no-tags.csv'delimiter ',' CSV HEADER;
--- COPY article_tag FROM '/Users/serranged/Documents/GitHub/pentacle/data/article_tags.csv'delimiter ',' CSV HEADER;
+CREATE TABLE education
+(
+    education_id serial primary key,
+    name         varchar(4000),
+    what         varchar(4000),
+    why          varchar(4000),
+    risk         varchar(4000),
+    reward       varchar(4000)
+);
+CREATE TABLE education_tag
+(
+    education_id int,
+    tag_id       int
+);
+COPY project FROM '/Users/pentaclexyz/projects/pentacle/data/project-no-tags.csv' delimiter ',' CSV HEADER;
+COPY tag FROM '/Users/pentaclexyz/projects/pentacle/data/tag.csv' delimiter ',' CSV HEADER;
+COPY project_tag FROM '/Users/pentaclexyz/projects/pentacle/data/project_tags.csv' delimiter ',' CSV HEADER;
+COPY article FROM '/Users/pentaclexyz/projects/pentacle/data/article-no-tags.csv' delimiter ',' CSV HEADER;
+COPY article_tag FROM '/Users/pentaclexyz/projects/pentacle/data/article_tags.csv' delimiter ',' CSV HEADER;
+COPY education FROM '/Users/pentaclexyz/projects/pentacle/data/education-no-tags.csv' delimiter ',' CSV HEADER;
+COPY education_tag FROM '/Users/pentaclexyz/projects/pentacle/data/education_tags.csv' delimiter ',' CSV HEADER;
---
+++ data/make-education-tags.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+# Set pandas display options
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+pd.set_option('display.width', 1500)
+
+dt = pd.read_csv("tag.csv", index_col="tag_id")
+tag_dict = dt.T.to_dict('records')[0]
+tag_dict = dict((v,k) for k,v in tag_dict.items())
+
+df = pd.read_csv("education.csv", index_col="education_id")
+
+map_df = df[['name', 'tag1', 'tag2']].copy()
+map_df['tag1'] = map_df['tag1'].map(tag_dict)
+map_df['tag2'] = map_df['tag2'].map(tag_dict)
+
+df1 = map_df[['tag1']].copy().dropna()
+df2 = map_df[['tag2']].copy().dropna()
+
+df1.columns = ['tag_id']
+df2.columns = ['tag_id']
+
+new_df = pd.concat([df1, df2], axis=0, sort=False)
+
+new_df['tag_id'] = new_df['tag_id'].astype(int)
+
+print(new_df)
+new_df.to_csv('education_tags.csv')
