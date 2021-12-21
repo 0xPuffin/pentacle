1fd6b0716ec6f9e3a6a478716d4f463100aea917
pentcle <pentacle@pentacle.xyz>
2021-12-21 13:39:38 +0000

    education db

--- server/app.js
+++ server/app.js
@@ -4,6 +4,11 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
+const ammRouter = require("./routes/education/amm");
+const divergenceLossRouter = require("./routes/education/divergence-loss");
+const holdRouter = require("./routes/education/hold");
+const stakeRouter = require("./routes/education/stake");
+const xykRouter = require("./routes/education/xyk");
 const ethCexRouter = require("./routes/centralised-exchange");
 const ethDexRouter = require("./routes/decentralised-exchange");
 const ethDefiProtocolRouter = require("./routes/yield");
@@ -22,7 +27,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/centralised-exchange", ethDexRouter);
+app.use("/amm", ammRouter);
+app.use("/divergence-loss", divergenceLossRouter);
+app.use("/hold", holdRouter);
+app.use("/stake", stakeRouter);
+app.use("/xyk", xykRouter);
+app.use("/centralised-exchange", ethCexRouter);
 app.use("/decentralised-exchange", ethDexRouter);
 app.use("/yield", ethDefiProtocolRouter);
 app.use("/insurance", ethDefiInsuranceRouter);
--- src/components/tags/education-tag-nav.js
+++ src/components/tags/education-tag-nav.js
@@ -1,13 +1,11 @@
 import React from 'react';
-// import React, {useEffect, useState} from 'react';
 import {Link} from "react-router-dom";
-// import Tags from './tags.component';
 
 export const EducationTagNav = () => {
 
     return (<nav>
             <article className={"nav-content overflow-scroll"}>
-                <Link to="/education-amm" className={"text-link padding-right-2"}>AMM</Link>
+                <Link to="/education-amm" className={"text-link padding-right-2"}>amm</Link>
                 <Link to="/education-divergence-loss" className={"text-link padding-right-2"}>Divergence loss</Link>
                 <Link to="/education-hold" className={"text-link padding-right-2"}>Hold</Link>
                 <Link to="/education-liquidity" className={"text-link padding-right-2"}>Liquidity</Link>
@@ -17,4 +15,3 @@ export const EducationTagNav = () => {
         </nav>
     );
 }
-
