9fb8851d7bac7bf6f01f270c2a6dadf6ab61b75b
pentcle <pentacle@pentacle.xyz>
2021-12-24 11:17:15 +0000

    navigation wip

--- src/pages/education/EducationDivergenceLossPage.js
+++ src/pages/education/EducationDivergenceLossPage.js
@@ -23,10 +23,24 @@ export function EducationDivergenceLossPage () {
             <Header/>
             <Layout>
                 <EducationTagNav/>
-                {/*{education && (*/}
-                {/*    <EducationContent {...education}/>*/}
-                <EducationContent data={education}/>
-                {/*)}*/}
+                {education && (
+                    <main className={"main-container"}>
+                        {education.map((data, index) => (
+                            <article className={"main-content margin-bottom-2"} key={index}>
+                                <h2>{data.education_name}</h2>
+                                <div className={"placeholder margin-bottom-1 margin-top-1"}>image thingy in here</div>
+                                <h3>What</h3>
+                                <p className={"large"}>{data.what}</p>
+                                <h3>Why</h3>
+                                <p className={"large"}>{data.why}</p>
+                                <h3>Reward</h3>
+                                <p className={"large"}>{data.reward}</p>
+                                <h3>Risk</h3>
+                                <p className={"large"}>{data.risk}</p>
+                            </article>
+                        ))}
+                    </main>
+                )}
             </Layout>
         </>
     );
