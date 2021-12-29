1bad0381ff6df0cd0c591dc422761b9914d7afc4
pentcle <pentacle@pentacle.xyz>
2021-12-29 15:48:48 +0000

    webpack

--- utils/webpack.config.js
+++ utils/webpack.config.js
@@ -1,32 +1,32 @@
-var webpack = require("webpack");
-
-module.export = {
-    entry: "./index.tsx",
-    mode: "development",
-    output: {
-        path: "build/assets",
-        filename: "bundle.js",
-        publicPath: "assets"
-    },
-
-    devServer: {
-        inline: true,
-        contentBase: "./build",
-        port: 3000
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                loader: "babel-loader"
-            },
-
-            {
-                test: /\.json$/,
-                exclude: /(node_modules)/,
-                loader: "json-loader"
-            }
-        ]
-    }
-};
+// var webpack = require("webpack");
+//
+// module.exports = {
+//     entry: "./index.tsx",
+//     mode: "development",
+//     output: {
+//         path: "build/assets",
+//         filename: "bundle.js",
+//         publicPath: "assets"
+//     },
+//
+//     devServer: {
+//         inline: true,
+//         contentBase: "./build",
+//         port: 3000
+//     },
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.js$/,
+//                 exclude: /(node_modules)/,
+//                 loader: "babel-loader"
+//             },
+//
+//             {
+//                 test: /\.json$/,
+//                 exclude: /(node_modules)/,
+//                 loader: "json-loader"
+//             }
+//         ]
+//     }
+// };
