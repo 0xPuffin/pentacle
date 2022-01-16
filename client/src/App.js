38a2c6319ab6dfa95934258b1b90ea31b19a751d
pentcle <pentacle@pentacle.xyz>
2022-01-16 00:12:08 +0000

    petals on the path for the llama
    Signed-off-by: pentcle <pentacle@pentacle.xyz>

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,6 @@ import Footer from "./components/footer/Footer";
 import {Router} from "./Router";
 
 // const themes = {
-//     light: 'pentacleui/src/scss/assets/styles/pentacle-light.scss',
-//     dark: 'pentacleui/src/scss/assets/styles/pentacle-light.scss',
-// };
-
 const App = () => (
         <BrowserRouter>
             <Router/>
