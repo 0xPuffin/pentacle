df4d3cb60776a75d07f3ec03e34b5f49e0cf9198
Alice <alize_in_cryptoland@protonmail.com>
2022-01-19 22:21:02 -0500

    fix top menu

--- client/src/components/navigation/Navigation.js
+++ client/src/components/navigation/Navigation.js
@@ -1,30 +1,48 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import ProjectsNavigation from "./ProjectsNavigation";
 import { SECTIONS } from "../../data/sections";
-import { SearchContext, SearchDispatchContext } from '../../providers/search-provider';
-import { NavLink } from 'react-router-dom';
+import {
+  SearchContext,
+  SearchDispatchContext,
+} from "../../providers/search-provider";
+import { NavLink } from "react-router-dom";
 
 function Navigation() {
   const { activeSection } = useContext(SearchContext);
   const { setActiveSection } = useContext(SearchDispatchContext);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <nav>
       <ul className={"navigation-content flex-center mt-10"}>
         <div className="dropdown">
-          <button tabIndex="0" className={`btn-ghost text-primary`}>
+          <button
+            onClick={() => setIsOpen(true)}
+            tabIndex="0"
+            className={`btn-ghost text-primary`}
+          >
             {activeSection}
           </button>
-          <ul
-            tabIndex="0"
-            className="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52"
-          >
-            {SECTIONS.map((section) => (
-              <li key={section} style={{ marginRight: 0 }}>
-                <NavLink to={`/${section}`} onClick={() => setActiveSection(section)}>{section}</NavLink>
-              </li>
-            ))}
-          </ul>
+          {isOpen && (
+            <ul
+              tabIndex="0"
+              className="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52"
+            >
+              {SECTIONS.map((section) => (
+                <li key={section} style={{ marginRight: 0 }}>
+                  <NavLink
+                    to={`/${section}`}
+                    onClick={() => {
+                      setIsOpen(false);
+                      setActiveSection(section);
+                    }}
+                  >
+                    {section}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
       </ul>
       <ProjectsNavigation />
