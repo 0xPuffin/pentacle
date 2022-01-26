390067344e7afef7a998fbce8fce8f7fcea542a3
pentcle <pentacle@pentacle.xyz>
2022-01-26 19:19:42 +0000

    react config etc

---
+++ packages/client/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname, "build")));
+
+// This route serves the React app
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
+
+
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('build'));
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.join('build', 'index.html'));
+//     });
+// }
