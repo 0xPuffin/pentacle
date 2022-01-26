cf040ad80d3c7d78a62aebf627b85bfc9a60eaf2
pentcle <pentacle@pentacle.xyz>
2022-01-26 18:06:20 +0000

    add prod and dev to config, add .env files to gitignore

--- packages/server/config.js
+++ packages/server/config.js
@@ -1,13 +1,27 @@
 const env = process.env;
 
-const config = {
+const prod = {
   db: {
     connectionString: process.env.DATABASE_URL,
     ssl: {
-      rejectUnauthorized: false,
-    },
+      rejectUnauthorized: false
+    }
   },
   listPerPage: env.LIST_PER_PAGE || 200,
 };
+
+const dev = {
+  db: {
+    host: env.DB_HOST || 'localhost',
+    port: env.DB_PORT || '5432',
+    user: env.DB_USER || 'postgres',
+    password: env.DB_PASSWORD || 'postgres',
+    database: env.DB_NAME || 'pentaclexyz',
+    ssl: false
+  },
+  listPerPage: env.LIST_PER_PAGE || 200,
+};
+
+const config = process.env.PROD === 'production' ? prod : dev;
 
 module.exports = config;
