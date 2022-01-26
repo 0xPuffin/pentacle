576135f592be1296484567ed184b2c4df2f25d72
pentcle <pentacle@pentacle.xyz>
2022-01-26 22:24:02 +0000

    Revert "take port info out of server"
    This reverts commit 61d36b856ee2107a669eb20e0f84d082cd70774e.

--- packages/client/server.js
+++ packages/client/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const port = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('build'));
@@ -8,3 +9,7 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.join('build', 'index.html'));
     });
 }
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
+
+
