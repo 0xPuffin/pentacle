2245f241c85aedb8dffcdeaa069a66be6bcd6886
pentcle <pentacle@pentacle.xyz>
2022-01-27 12:23:02 +0000

    update server

--- packages/client/server.js
+++ packages/client/server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT;
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
 
