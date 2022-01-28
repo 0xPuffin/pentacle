7b53220378593483b08c9be14fc888ada065c288
Alice <alize_in_cryptoland@protonmail.com>
2022-01-27 20:05:55 -0500

    use cors

--- packages/server/app.js
+++ packages/server/app.js
@@ -30,6 +30,30 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// CORS
+var cors = require("cors");
+var allowedOrigins = [
+  "http://localhost:3000",
+  "https://pentacle-server.herokuapp.com"
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
+
 app.use("/", indexRouter);
 
 app.use("/articles/articles", articlesRouter);
