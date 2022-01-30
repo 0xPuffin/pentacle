5b471674c9586a5b0dfa28401dde2fedb1c56cd4
pentcle <pentacle@pentacle.xyz>
2022-01-30 17:24:52 +0000

    remove server dir

--- server/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', function(req, res, next) {
-    res.json({message: "Hello from le server"});
-});
-
-module.exports = router;
