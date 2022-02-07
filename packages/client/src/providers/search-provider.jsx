394e1a8db19d944b60ac58fa0b0467550e6e04ba
pentcle <pentacle@pentacle.xyz>
2022-02-07 11:55:34 +0000

    update path for api app name
    Signed-off-by: pentcle <pentacle@pentacle.xyz>

--- packages/client/src/providers/search-provider.jsx
+++ packages/client/src/providers/search-provider.jsx
@@ -17,7 +17,7 @@ export const SearchDispatchContext = createContext({
 
 export const SearchContext = createContext(initVal);
 
-const BASE_URI = process.env.NODE_ENV === 'production' ? (`${process.env.API_APP_NAME}`) : '/api'
+const BASE_URI = process.env.NODE_ENV === 'production' ? `${process.env.API_APP_NAME}` : '/api'
 
 export function SearchProvider({ children }) {
   const [projectsLoading, setProjectsLoading] = useState(true);
