efdc423801ac3acaa2be06cad25b09bf745f46b2
pentcle <pentacle@pentacle.xyz>
2022-02-15 11:52:02 +0000

    top nav
    Signed-off-by: pentcle <pentacle@pentacle.xyz>

--- packages/client/src/components/project-header/ProjectHeader.tsx
+++ packages/client/src/components/project-header/ProjectHeader.tsx
@@ -1,19 +1,21 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import Header from "../header/Header";
 
 const ProjectHeader = () => {
-    return (
-      <Header>
-        {/* 
+    return (<Header>
+            <nav>
+                <ul>
+                {/*
                 @pentacle: for styling: each of these 'active' when we are on their route
                 https://v5.reactrouter.com/web/api/NavLink
                 */}
-        <NavLink to="/projects">Projects</NavLink>
-        <NavLink to="/education">Education</NavLink>
-        <NavLink to="/articles">Articles</NavLink>
-        <NavLink to="/events">Events</NavLink>
-      </Header>
-    );
-  };
-  export default ProjectHeader;
+                    <li><NavLink to="/projects">Projects</NavLink></li>
+                    <li><NavLink to="/education">Education</NavLink></li>
+                    <li><NavLink to="/articles">Articles</NavLink></li>
+                    <li><NavLink to="/events">Events</NavLink></li>
+                </ul>
+            </nav>
+        </Header>);
+};
+export default ProjectHeader;
