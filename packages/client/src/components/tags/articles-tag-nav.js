099ec3f8e47d15ad1e80e6c20c9487d7246ce506
pentcle <pentacle@pentacle.xyz>
2022-02-18 23:42:50 +0000

    some styling
    Signed-off-by: pentcle <pentacle@pentacle.xyz>

---
+++ packages/client/src/components/tags/articles-tag-nav.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Tags from "./tags.component";
+
+export const ArticlesTagNav = () => {
+  const [loading, setLoading] = useState(true);
+  const [tags, setTags] = useState([]);
+
+  const fetchTags = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_SERVER_URI}/articles/tags`
+      );
+      const res = await response.json();
+      setLoading(false);
+      setTags(res.data);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchTags();
+  }, []);
+
+  if (loading) {
+    return (
+      <nav className={"nav-content"}>
+        <Link to={""}>Loading..</Link>
+      </nav>
+    );
+  }
+  return (
+    <nav>
+      <article className={"nav-content overflow-scroll"}>
+        <Tags tags={tags} />
+      </article>
+    </nav>
+  );
+};
--- packages/client/src/pages/articles/articles.js
+++ packages/client/src/pages/articles/articles.js
@@ -1,30 +1,26 @@
 import React, {useContext} from "react";
 import Layout from "../../components/layouts/layout";
-import { ProjectTagNav } from "../../components/tags/project-tag-nav";
 import HeaderV2 from "../../components/header-v2/HeaderV2";
-import { SearchContext } from "../../providers/search-provider";
+import {SearchContext} from "../../providers/search-provider";
+import {ArticlesTagNav} from "../../components/tags/articles-tag-nav";
 
 export const ArticlesPage = () => {
-  const { pageData } = useContext(SearchContext);
-  return (
-    <>
-      <HeaderV2 />
-      <Layout>
-          <ProjectTagNav />
-          {pageData && (
-            <main className={"main-container"}>
-              <section className={"main-content"}>
-                <h1 className={"boxed"}>articles</h1>
-                {pageData.map((data, index) => (
-                  <article key={index}>
-                    <h3>{data.article_name}</h3>
-                    <a href={data.article_url}>{data.article_url}</a>
-                  </article>
-                ))}
-              </section>
-            </main>
-        )}
-      </Layout>
-    </>
-  );
+    const {pageData} = useContext(SearchContext);
+    return (<>
+            <HeaderV2/>
+            <Layout>
+                {pageData && (<main className={"main-container"}>
+                        <section className={"main-content"}>
+                            <article className={"flex space-between"}>
+                                <h1 className={"boxed"}>articles</h1>
+                                <ArticlesTagNav/>
+                            </article>
+                            {pageData.map((data, index) => (<article key={index}>
+                                    <h3>{data.article_name}</h3>
+                                    <a href={data.article_url}>{data.article_url}</a>
+                                </article>))}
+                        </section>
+                    </main>)}
+            </Layout>
+        </>);
 };
