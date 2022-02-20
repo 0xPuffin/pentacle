6a5df361d501fd48849cd8afcfb126d4d4cb34ad
pentcle <pentacle@pentacle.xyz>
2022-02-20 18:17:41 +0000

    start events
    Signed-off-by: pentcle <pentacle@pentacle.xyz>

--- packages/client/src/pages/events/events.js
+++ packages/client/src/pages/events/events.js
@@ -16,11 +16,11 @@ export function EventsPage() {
                         {/*<EventsTagNav/>*/}
                     </article>
                     <article className={"flex margin-top-2"}>
-                    {pageData.map((data, index) => (<div className={"p-card margin-bottom-2"} key={index}>
-                        <h3>{data.event_name}</h3>
+                    {pageData.map((data, index) => (<div className={"p-card margin-bottom-1"} key={index}>
+                        <h3 className={"boxed lowercase"}>{data.event_name}</h3>
                         <p>{data.start} - {data.finish}</p>
                         <p>{data.location}</p>
-                        <p><a className={"text-link"} href={`http://${data.event_url}`}>{data.event_url}</a></p>
+                        <p><a className={"text-link"} href={`http://${data.event_url}`} target={"_blank"} rel="noopener noreferrer">{data.event_url}</a></p>
                         <p>{data.twitter}</p>
                         <p>{data.start}</p>
                     </div>))}
