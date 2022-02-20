025aa5f8132bee0709b1f3c5cfc968905383dedf
pentcle <pentacle@pentacle.xyz>
2022-02-20 17:27:02 +0000

    linky
    Signed-off-by: pentcle <pentacle@pentacle.xyz>

--- packages/client/src/components/header/Header.js
+++ packages/client/src/components/header/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
             </div>
             <nav className={"flex flex-end"}>
                 <ul className={"hello"}>
-                    <li><NavLink to="/projects">projects</NavLink> (beta)</li>
+                    <li><NavLink to="/projects" className={"text-link"}>projects</NavLink> (beta)</li>
                     {/*<li><NavLink to="/education">education</NavLink></li>*/}
                     {/*<li><NavLink to="/articles">articles</NavLink></li>*/}
                     {/*<li><NavLink to="/events">events</NavLink></li>*/}
