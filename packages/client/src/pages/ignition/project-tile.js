9b932ffb5ac9b1ef2db9a8f34dc69cb51c151449
pentcle <pentacle@pentacle.xyz>
2022-02-20 12:08:26 +0000

    pain
    Signed-off-by: pentcle <pentacle@pentacle.xyz>

--- packages/client/src/pages/ignition/project-tile.js
+++ packages/client/src/pages/ignition/project-tile.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ProjectTile = ({ name, description, onClick }) => {
   return (
-    <article className="" onClick={onClick}>
+    <article className="project-tile box link secondary dark" onClick={onClick}>
       <h3>{name}</h3>
       <p className={"length-300"}>{description}</p>
     </article>
