6444e7f9cacb859e24c2836da64f7c9ad7ef001d
pentcle <pentacle@pentacle.xyz>
2022-02-20 18:45:02 +0000

    pink
    Signed-off-by: pentcle <pentacle@pentacle.xyz>

--- packages/client/src/pages/events/events.js
+++ packages/client/src/pages/events/events.js
@@ -20,7 +20,7 @@ export function EventsPage() {
                         <h3 className={"boxed lowercase"}>{data.event_name}</h3>
                         <p>{data.start} - {data.finish}</p>
                         <p>{data.location}</p>
-                        <p><a className={"text-link"} href={`http://${data.event_url}`} target={"_blank"} rel="noopener noreferrer">{data.event_url}</a></p>
+                        <p><a className={"text-link"} href={`https://${data.event_url}`} target={"_blank"} rel="noopener noreferrer">{data.event_url}</a></p>
                         <p>{data.twitter}</p>
                         <p>{data.start}</p>
                     </div>))}
