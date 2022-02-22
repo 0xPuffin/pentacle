f0230fddff1272d27bda91d7694f4111fc0c3828
pentcle <pentacle@pentacle.xyz>
2022-02-22 14:57:56 +0000

    education wip
    Signed-off-by: pentcle <pentacle@pentacle.xyz>

--- packages/client/src/components/project-layout/EducationSummary.js
+++ packages/client/src/components/project-layout/EducationSummary.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 const ProjectSummary = ({education_name, description, onClick}) => {
-    return (<article className={"p-card"}  onClick={onClick}>
-        <h3 className={"boxed"}>{education_name}</h3>
+    return (<article className={"p-card small"}  onClick={onClick}>
+        <h3 className={"boxed lowercase"}>{education_name}</h3>
         <p className={"length-300"}>{description}</p>
     </article>);
 };
