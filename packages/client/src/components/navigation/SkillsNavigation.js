c5e77450cd00857660988209239c5a9157f59c02
pentcle <pentacle@pentacle.xyz>
2022-02-26 14:32:47 +0000

    skills
    Signed-off-by: pentcle <pentacle@pentacle.xyz>

---
+++ packages/client/src/components/navigation/SkillsNavigation.js
@@ -0,0 +1,22 @@
+import React, { useContext } from "react";
+import { SearchContext, SearchDispatchContext } from "../../providers/search-provider";
+import { NavLink } from "react-router-dom";
+import {SKILL_CATEGORIES} from "../../data/categories";
+
+function EducationNavigation() {
+  const { activeSection } = useContext(SearchContext);
+  const { setActiveCategory } = useContext(SearchDispatchContext);
+  return (
+    <nav className="flex flex-end">
+      <ul className={"navigation-content"}>
+        {SKILL_CATEGORIES.map((category, index) => (
+          <li key={index}>
+            <NavLink to={`/${activeSection}/${category}`} onClick={() => setActiveCategory(category)}>{category}</NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+export default EducationNavigation;
