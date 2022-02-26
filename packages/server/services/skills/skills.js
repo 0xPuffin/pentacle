794f39d2904a16f4bfca2a81d4a050156e5e3daa
pentcle <pentacle@pentacle.xyz>
2022-02-26 22:30:14 +0000

    well will have to ask for help after this
    Signed-off-by: pentcle <pentacle@pentacle.xyz>

--- packages/server/services/skills/skills.js
+++ packages/server/services/skills/skills.js
@@ -9,7 +9,7 @@ async function getMultiple(page = 1) {
       "skill.title AS skill_title, " +
       "skill.text AS skill_text, " +
       "tag.name AS tag_name " +
-      "REPLACE(text, '\n', '\r') " +
+      "REPLACE(skill.text, '\n', '\r') " +
       "FROM skill " +
       "JOIN skill_tag " +
       "ON skill.skill_id = skill_tag.skill_id " +
