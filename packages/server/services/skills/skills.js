c228076486e3713db688cdbad5bdf5e80573466d
pentcle <pentacle@pentacle.xyz>
2022-02-26 22:00:44 +0000

    pain
    Signed-off-by: pentcle <pentacle@pentacle.xyz>

--- packages/server/services/skills/skills.js
+++ packages/server/services/skills/skills.js
@@ -9,6 +9,8 @@ async function getMultiple(page = 1) {
       "skill.title AS skill_title, " +
       "skill.text AS skill_text, " +
       "tag.name AS tag_name " +
+      "UPDATE skill " +
+      "SET skill.text = REPLACE (skill.text, '\n', '\r') " +
       "FROM skill " +
       "JOIN skill_tag " +
       "ON skill.skill_id = skill_tag.skill_id " +
@@ -16,8 +18,6 @@ async function getMultiple(page = 1) {
       "ON tag.tag_id = skill_tag.tag_id " +
       "WHERE tag.name = 'skill' " +
       "ORDER BY skill.title ASC " +
-      "UPDATE skill " +
-      "SET skill.text = REPLACE (skill.text, '\n', '\r') " +
       "OFFSET $1 LIMIT $2",
     [offset, config.listPerPage]
   );
