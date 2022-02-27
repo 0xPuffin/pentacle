6082dc33e69c47e7894110beae00da614721ddf2
pentcle <pentacle@pentacle.xyz>
2022-02-27 15:41:08 +0000

    come on
    Signed-off-by: pentcle <pentacle@pentacle.xyz>

--- packages/client/src/components/project-layout/SkillDetail.tsx
+++ packages/client/src/components/project-layout/SkillDetail.tsx
@@ -2,15 +2,15 @@ import React from "react";
 
 type Props = {
     skill_title: string;
-    skill_text: string;
+    replace: string;
 };
 
-const SkillDetail = ({skill_title, skill_text}: Props) => {
+const SkillDetail = ({skill_title, replace}: Props) => {
     return (<section className={"main-content margin-top-2"}>
         <article className={"margin-bottom-2 readability-max-width"}>
             <h2>{skill_title}</h2>
             <p>by <a href={"https://twitter.com/knowerofmarkets"} target={"_blank"} rel="noopener noreferrer">@knowerofmarkets</a></p>
-            <p className={"large"} style={{whiteSpace: "pre-wrap"}}>{skill_text}</p>
+            <p className={"large"} style={{whiteSpace: "pre-wrap"}}>{replace}</p>
         </article>
     </section>);
 };
--- packages/server/services/skills/skills.js
+++ packages/server/services/skills/skills.js
@@ -9,7 +9,7 @@ async function getMultiple(page = 1) {
       "skill.title AS skill_title, " +
       "skill.text AS skill_text, " +
       "tag.name AS tag_name, " +
-      "REPLACE(skill.text, '\\n', '\n') " +
+      "SET skill.text = REPLACE(skill.text, '\\n', '\n') " +
       "FROM skill " +
       "JOIN skill_tag " +
       "ON skill.skill_id = skill_tag.skill_id " +
