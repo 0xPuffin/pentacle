fbe86138e71d8a642d594a349ca7f71e8d559a44
pentcle <pentacle@pentacle.xyz>
2022-02-28 13:53:04 +0000

    one tries
    Signed-off-by: pentcle <pentacle@pentacle.xyz>

--- packages/server/services/skills/skills.js
+++ packages/server/services/skills/skills.js
@@ -8,7 +8,7 @@ async function getMultiple(page = 1) {
       "SELECT skill.skill_id, skill.title AS skill_title, skill.text AS skill_text, tag.name AS tag_name " +
       "REPLACE(skill.text, '\\n', '\n') " +
       "FROM skill " +
-      "WHERE skill.text NOT NULL " +
+      "WHERE skill.text IS NOT NULL " +
       "JOIN skill_tag " +
       "ON skill.skill_id = skill_tag.skill_id " +
       "JOIN tag " +
