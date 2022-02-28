a2494b0a2d9a1d574c7fd7aff6e3c42c2c7a3a06
pentcle <pentacle@pentacle.xyz>
2022-02-28 13:39:04 +0000

    one tries
    Signed-off-by: pentcle <pentacle@pentacle.xyz>

--- packages/server/services/skills/skills.js
+++ packages/server/services/skills/skills.js
@@ -5,13 +5,10 @@ const config = require("../../config");
 async function getMultiple(page = 1) {
   const offset = helper.getOffset(page, config.listPerPage);
   const rows = await db.query(
-      "SELECT skill.skill_id, " +
-      "skill.title AS skill_title, " +
-      "skill.text AS skill_text, " +
-      "tag.name AS tag_name, " +
+      "SELECT skill.skill_id, skill.title AS skill_title, skill.text AS skill_text, tag.name AS tag_name " +
       "REPLACE(skill.text, '\\n', '\n') " +
       "FROM skill " +
-      "WHERE skill.text IS NOT NULL, " +
+      "WHERE skill.text IS NOT NULL " +
       "JOIN skill_tag " +
       "ON skill.skill_id = skill_tag.skill_id " +
       "JOIN tag " +
