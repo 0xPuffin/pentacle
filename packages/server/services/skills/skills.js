ff34ef372c8970cd4926443dc82c3889bb9d8199
pentcle <pentacle@pentacle.xyz>
2022-02-28 13:54:33 +0000

    maybe needed reformatting :shrug:
    Signed-off-by: pentcle <pentacle@pentacle.xyz>

--- packages/server/services/skills/skills.js
+++ packages/server/services/skills/skills.js
@@ -3,64 +3,64 @@ const helper = require("../../helper");
 const config = require("../../config");
 
 async function getMultiple(page = 1) {
-  const offset = helper.getOffset(page, config.listPerPage);
-  const rows = await db.query(
-      "SELECT skill.skill_id, skill.title AS skill_title, skill.text AS skill_text, tag.name AS tag_name " +
-      "REPLACE(skill.text, '\\n', '\n') " +
-      "FROM skill " +
-      "WHERE skill.text IS NOT NULL " +
-      "JOIN skill_tag " +
-      "ON skill.skill_id = skill_tag.skill_id " +
-      "JOIN tag " +
-      "ON tag.tag_id = skill_tag.tag_id " +
-      "WHERE tag.name = 'skill' " +
-      "ORDER BY skill.title ASC " +
-      "OFFSET $1 LIMIT $2",
-    [offset, config.listPerPage]
-  );
-  const data = helper.emptyOrRows(rows);
-  const meta = { page };
+    const offset = helper.getOffset(page, config.listPerPage);
+    const rows = await db.query(
+        "SELECT skill.skill_id, skill.title AS skill_title, skill.text AS skill_text, tag.name AS tag_name " +
+        "REPLACE(skill.text, '\\n', '\n') " +
+        "FROM skill " +
+        "WHERE skill.text IS NOT NULL " +
+        "JOIN skill_tag " +
+        "ON skill.skill_id = skill_tag.skill_id " +
+        "JOIN tag " +
+        "ON tag.tag_id = skill_tag.tag_id " +
+        "WHERE tag.name = 'skill' " +
+        "ORDER BY skill.title ASC " +
+        "OFFSET $1 LIMIT $2",
+        [offset, config.listPerPage]
+    );
+    const data = helper.emptyOrRows(rows);
+    const meta = {page};
 
-  return {
-    data,
-    meta,
-  };
+    return {
+        data,
+        meta,
+    };
 }
 
 function validateCreate(education) {
-  let messages = [];
+    let messages = [];
 
-  console.log(education);
+    console.log(education);
 
-  if (!education) {
-    messages.push("No object is provided");
-  }
+    if (!education) {
+        messages.push("No object is provided");
+    }
 
-  if (messages.length) {
-    let error = new Error(messages.join());
-    error.statusCode = 400;
+    if (messages.length) {
+        let error = new Error(messages.join());
+        error.statusCode = 400;
 
-    throw error;
-  }
+        throw error;
+    }
 }
 
 async function create(education) {
-  validateCreate(education);
+    validateCreate(education);
 
-  const result = await db.query(
-    "INSERT INTO education(name) VALUES (bob) RETURNING *",
-    [education.name]
-  );
-  let message = "Error in creating education";
+    const result = await db.query(
+        "INSERT INTO education(name) VALUES (bob) RETURNING *",
+        [education.name]
+    );
+    let message = "Error in creating education";
 
-  if (result.length) {
-    message = "Project created successfully";
-  }
+    if (result.length) {
+        message = "Project created successfully";
+    }
 
-  return { message };
+    return {message};
 }
 
 module.exports = {
-  getMultiple,
-  create,
+    getMultiple,
+    create,
 };
