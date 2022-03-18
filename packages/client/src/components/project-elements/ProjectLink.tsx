2cea139e89d32fec49179feabe328e38347176b7
pentcle <pentacle@pentacle.xyz>
2022-03-18 21:18:55 +0000

    let's hope i didn't break anything
    Signed-off-by: pentcle <pentacle@pentacle.xyz>

--- packages/client/src/components/project-elements/ProjectLink.tsx
+++ packages/client/src/components/project-elements/ProjectLink.tsx
@@ -3,16 +3,7 @@ import React from "react";
 const ProjectLink = (props: any) => {
   return (
     <li className={"inline-block margin-right-1"}>
-      {props && (
-        <a
-          href={props.url}
-          className={"text-link underline"}
-          target={"_blank"}
-          rel="noopener noreferrer"
-        >
-          {props.title}
-        </a>
-      )}
+      {props && (<a href={props.url} className={"text-link"} target={"_blank"} rel="noopener noreferrer">{props.title}</a>)}
     </li>
   );
 };
--- packages/client/src/pages/ignition/toplevel.js
+++ packages/client/src/pages/ignition/toplevel.js
@@ -15,7 +15,7 @@ export const TopLevel = ({ projects }) => {
       <nav className={"margin-top-2"}>
         <ul className={"nav-content overflow-x-scroll"}>
           {projects.map((project, index) => (
-            <li key={index}><a className={"text-link padding-right-2"} onClick={() => setProjectDetail(projects[index])}>{project.name}</a></li>
+            <li key={index}><a className={"text-link"} onClick={() => setProjectDetail(projects[index])}>{project.name}</a></li>
           ))}
         </ul>
       </nav>
