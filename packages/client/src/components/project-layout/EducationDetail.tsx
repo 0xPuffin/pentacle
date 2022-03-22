fed9a8eaa8d5507cef6cc5731c4778aa9632a866
pentcle <pentacle@pentacle.xyz>
2022-03-22 03:49:18 +0000

    we
    Signed-off-by: pentcle <pentacle@pentacle.xyz>

--- packages/client/src/components/project-layout/EducationDetail.tsx
+++ packages/client/src/components/project-layout/EducationDetail.tsx
@@ -12,7 +12,6 @@ const EducationDetail = ({education_name, what, why, reward, risk}: Props) => {
     return (<section className={"main-content"}>
         <article className={"margin-bottom-2 readability-max-width"}>
             <h2>{education_name}</h2>
-            <p>by <a href={"https://twitter.com/noidtwo"} target={"_blank"} rel="noopener noreferrer">@noidtwo</a></p>
             <h3 className={"lowercase"}>what</h3>
             <p className={"large"}>{what}</p>
             <h3 className={"lowercase"}>why</h3>
