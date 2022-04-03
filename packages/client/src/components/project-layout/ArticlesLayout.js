6b27efcacf1ce93b12b850feff382b24d5266b31
pentcle <pentacle@pentacle.xyz>
2022-04-03 19:13:30 +0100

    articles

--- packages/client/src/components/project-layout/ArticlesLayout.js
+++ packages/client/src/components/project-layout/ArticlesLayout.js
@@ -7,7 +7,7 @@ export const ArticlesLayout = ({projects = []}) => {
             {projects.map((project, index) => (<a key={index} id={`item-${index}`} href={project.article_url} rel="noopener noreferrer" target={"_blank"}>
                 <article className="p-card wide beginner margin-bottom-1">
                     <h3>{project.article_name}</h3>
-                    <span className={"text-link"}>{project.article_url}</span>
+                    <a className={"text-link"}>{project.article_url}</a>
                 </article>
             </a>))}
         </section>
