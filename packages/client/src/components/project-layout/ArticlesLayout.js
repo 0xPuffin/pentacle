e64c62fa046b67bb8702ed7724d50bc47361c76e
pentcle <pentacle@pentacle.xyz>
2022-04-03 15:00:03 +0100

    articles

--- packages/client/src/components/project-layout/ArticlesLayout.js
+++ packages/client/src/components/project-layout/ArticlesLayout.js
@@ -6,7 +6,7 @@ export const ArticlesLayout = ({projects = []}) => {
 
     return (<>
         <section className={"main-content margin-top-1"}>
-                {projects.map((project, index) => (<article key={index} id={`item-${index}`} className="p-card wide beginner margin-bottom-2">
+                {projects.map((project, index) => (<article key={index} id={`item-${index}`} className="p-card wide beginner margin-bottom-1">
                     <h3>{project.article_name}</h3>
                     <a className={"text-link"} href={project.article_url}>{project.article_url}</a>
                 </article>))}
